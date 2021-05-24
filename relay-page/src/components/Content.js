695ed32693e7349c712366ba8768af0cef7625be
victoria.k <victoria.k@kakaocorp.com>
2021-05-25 02:59:55 +0900

    content 스타일 업데이트

--- relay-page/src/components/Content.js
+++ relay-page/src/components/Content.js
@@ -8,10 +8,10 @@ const Content = () => (
             </div>
             <div className="description">
                 <ul>
-                    <li className="content_list"> 우리는 각자의 경험을 나누고 함께 발전하는것을 좋아합니다. </li>
-                    <li className="content_list"> 막히는 것이 있다면 언제든 편하게 같이 이야기합니다. </li>
-                    <li className="content_list"> 각자의 자리에서 가진 고민들을 함께 나누며 성장하고 있습니다.</li>
-                    <li className="content_list"> 앗, 물론 개발만이 아니라 함께 놀기도 합니다!! </li>
+                    <li> 우리는 각자의 경험을 나누고 함께 발전하는것을 좋아합니다. </li>
+                    <li> 막히는 것이 있다면 언제든 편하게 같이 이야기합니다. </li>
+                    <li> 각자의 자리에서 가진 고민들을 함께 나누며 성장하고 있습니다.</li>
+                    <li> 앗, 물론 개발만이 아니라 함께 놀기도 합니다!! </li>
                 </ul>
             </div>
         </div>
