c32060fbdecf7af34ecd04d77b6bada58ac6d75f
victoria.k <victoria.k@kakaocorp.com>
2021-05-25 01:40:17 +0900

    헤더 추가 및 css 초기화

--- relay-page/src/components/Footer.js
+++ relay-page/src/components/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react'; 
 
 const Footer = () => ( 
-    <div className="footer">
-        Copyright © Ausg. All rights reserved.
-    </div>
+    <footer class="doc_footer">
+        <div class="wrap_comm">
+            <h2 class="screen_out">Contact Us : </h2>
+            <small class="txt_copyright">Copyright © <a href="#none" target="_blank" class="link_kakao">Ausg.</a> All rights reserved</small>
+        </div>
+    </footer>
 )
 
 export default Footer;
--- relay-page/src/components/Header.js
+++ relay-page/src/components/Header.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Header = () => (
     <div className="header">
-        WEBTOON
+        <div class="doc_header">
+            <h1 class="doc_title">
+                <a href="/" class="link_logo">
+                  <img src="/static/img/img-nft-exchange.svg" alt="NFT Exchange" class="img_logo" />
+                </a>
+            </h1>
+        </div>
     </div>
-
 )
 
 export default Header;
