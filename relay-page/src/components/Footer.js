52910bd092a11cc88141284d7aabee1af36e2fe2
victoria.k <victoria.k@kakaocorp.com>
2021-05-25 01:54:04 +0900

    Header, Footer 디자인 적용

--- relay-page/src/components/Footer.js
+++ relay-page/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => ( 
     <footer class="doc_footer">
         <div class="wrap_comm">
-            <h2 class="screen_out">Contact Us : </h2>
+            <h4 class="list_policy">Contact Us : </h4>
             <small class="txt_copyright">Copyright © <a href="#none" target="_blank" class="link_kakao">Ausg.</a> All rights reserved</small>
         </div>
     </footer>
--- relay-page/src/components/Header.js
+++ relay-page/src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+// import logosvg from '../images/logo-white.svg';
 
 const Header = () => (
     <div className="header">
         <div class="doc_header">
             <h1 class="doc_title">
                 <a href="/" class="link_logo">
-                  <img src="/static/img/img-nft-exchange.svg" alt="NFT Exchange" class="img_logo" />
+                  <img src="/images/logo-white.svg" alt="AUSG" class="img_logo" />
                 </a>
             </h1>
         </div>
