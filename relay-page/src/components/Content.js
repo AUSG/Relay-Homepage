477c70c3cd96f0b5f186e5806a42125e38bb9c94
victoria.k <victoria.k@kakaocorp.com>
2021-05-25 03:17:52 +0900

    메인 소개 컨텐츠 추가

--- relay-page/src/components/Content.js
+++ relay-page/src/components/Content.js
@@ -4,10 +4,31 @@ const Content = () => (
     <div className="main">
         <div className="doc_content">
             <div className="title">
-                <strong> Our Vision </strong>
+                <strong> AWSkrug University Student Group </strong>
             </div>
             <div className="description">
                 <ul>
+                    <li> AUSG는  AWS와 클라우드, 프로그래밍에 관심을 갖고 </li>
+                    <li> 이와 관련 주제들로 이야기를 나누고  </li>
+                    <li> 함께 성장하며 나아가는 대학생들의 모임입니다. </li>
+                </ul>
+
+                <ul>
+                    <li> AUSG에서는 대학교에서 공부하는 친구들부터 현업에서 근무하는 개발자도 함께 </li>
+                    <li> 다양한 사람들을 만나 소통하실 수 있습니다. </li>
+                    <li> 함께 성장하며 나아가는 대학생들의 모임입니다. </li>
+                </ul>
+
+                <ul>
+                    <li> AWS 한국사용자모임(https://awskrug.github.io/)과 함께하고 있어요! 😎 </li>
+                </ul>
+            </div>
+
+            <div className="title">
+                <strong> Our Vision </strong>
+            </div>
+            <div className="description">
+                <ul className="square_list_type">
                     <li> 우리는 각자의 경험을 나누고 함께 발전하는것을 좋아합니다. </li>
                     <li> 막히는 것이 있다면 언제든 편하게 같이 이야기합니다. </li>
                     <li> 각자의 자리에서 가진 고민들을 함께 나누며 성장하고 있습니다.</li>
