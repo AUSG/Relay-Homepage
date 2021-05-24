2b76b79dfb98525e96459545ffef93954d9d7e27
victoria.k <victoria.k@kakaocorp.com>
2021-05-25 01:27:48 +0900

    프로젝트 구조 잡기

---
+++ relay-page/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+const Footer = () => ( 
+    <div className="footer">
+        Copyright © Ausg. All rights reserved.
+    </div>
+)
+
+export default Footer;
---
+++ relay-page/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+    <div className="header">
+        WEBTOON
+    </div>
+
+)
+
+export default Header;
