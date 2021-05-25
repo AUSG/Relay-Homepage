437fa6ffd625b21bb08a935d6d2b1a4c18a74cf9
rayleighko <ray@scentlab.kr>
2021-05-25 14:53:33 +0900

    fix: app

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "../styles/global.css";
 import { AppProps } from "next/app";
 import React from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
-}
+};
+export default App;
