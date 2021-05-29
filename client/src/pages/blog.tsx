1d3d9554b979601594384611fd8e3e0a22fafaa3
Roeniss Moon <roeniss2@gmail.com>
2021-05-29 22:06:48 +0900

    feat: blog page UI markup (NO mobile-compatibile)

--- client/src/pages/blog.tsx
+++ client/src/pages/blog.tsx
@@ -1,3 +1,4 @@
+import BlogCard from "@ausg/components/molecules/BlogCard";
 import Intro from "@ausg/components/molecules/Intro";
 import AppLayout from "@ausg/components/templates/AppLayout";
 
@@ -8,6 +9,66 @@ const BlogPage = () => {
         title="AUSG Blog"
         description="AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요"
       />
+      {/* <div className="grid-cols-4 sm:grid-cols-1"> */}
+
+      {/* <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12"> */}
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 justify-items-center mx-40">
+        <BlogCard
+          title="asdjkhaskdjhas"
+          preface="AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 "
+          url={"https://velog.io/"}
+          author="문성혁"
+        />
+        <BlogCard
+          title="asdjkhaskdjhas"
+          url={"https://medium.com/"}
+          author="문성혁"
+        />
+        <BlogCard
+          title="asdjkhaskdjhas"
+          preface="AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 "
+          url={"https://velog.io/"}
+          author="문성혁"
+        />
+        <BlogCard
+          title="asdjkhaskdjhas"
+          preface="AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 "
+          url={"https://velog.io/"}
+          author="문성혁"
+        />
+        <BlogCard
+          title="asdjkhaskdjhas"
+          preface="AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 "
+          url={"https://velog.io/"}
+          author="문성혁"
+        />
+        <BlogCard
+          title="asdjkhaskdjhas"
+          preface="AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 "
+          url={"https://velog.io/"}
+          author="문성혁"
+        />
+        <BlogCard
+          title="asdjkhaskdjhas"
+          preface="AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 "
+          url={"https://asd.io/"}
+          author="문성혁"
+        />
+        <BlogCard
+          title="asdjkhaskdjhas"
+          preface="AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 "
+          url={"https://roeniss.tistory.com/"}
+          author="문성혁"
+        />
+        <BlogCard
+          title="asdjkhaskdjhas"
+          preface="AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 "
+          url={"https://velog.io/"}
+          author="문성혁"
+        />
+        {/* </div> */}
+      </div>
+      {/* </div> */}
     </AppLayout>
   );
 };
