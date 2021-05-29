3f0e92ae95e2469355d906a6a51b999004bceefa
SEONGIK KIM <kpic1638@gmail.com>
2021-05-29 19:25:23 +0900

    <feat> Team Component - People page

--- client/src/pages/people.tsx
+++ client/src/pages/people.tsx
@@ -1,14 +1,17 @@
 import Intro from "@ausg/components/molecules/Intro";
+import Team from "@ausg/components/people/Team";
 import AppLayout from "@ausg/components/templates/AppLayout";
+
 
 const PeoplePage = () => {
   return (
     <AppLayout>
       <Intro
         title="AUSG People"
-        description="저마다 개성 넘치는 AUSG 크루들을 만나보세요"
+        description="저마다 개성 넘치는 AUSG 크루들을 만나보세요."
       />
-    </AppLayout>
+      <Team />
+    </AppLayout >
   );
 };
 
