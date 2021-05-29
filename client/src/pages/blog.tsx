41349fe3229a7b2158bb9bd64404d0db19e18236
Roeniss Moon <roeniss2@gmail.com>
2021-05-30 01:56:52 +0900

    fix: improvise UI markup, add fetch function to blog list data

--- client/src/pages/blog.tsx
+++ client/src/pages/blog.tsx
@@ -1,74 +1,61 @@
 import BlogCard from "@ausg/components/molecules/BlogCard";
 import Intro from "@ausg/components/molecules/Intro";
 import AppLayout from "@ausg/components/templates/AppLayout";
+import useAxios from "axios-hooks";
+
+import ReactLoading from "react-loading";
 
 const BlogPage = () => {
+  const [{ data, loading, error }, refetch] = useAxios(
+    "https://raw.githubusercontent.com/AUSG/Relay-Homepage/feature/blogPage/lib/blogSpider/newestPosts.csv",
+  );
+
+  let mainComponent;
+  if (loading) {
+    mainComponent = (
+      <div className="grid grid-cols-1 justify-items-center mx-40">
+        <ReactLoading
+          type={"spin"}
+          color={"#6b66ff"}
+          height={"5%"}
+          width={"5%"}
+        />
+      </div>
+    );
+  } else if (error) {
+    mainComponent = (
+      <div className="grid grid-cols-1 justify-items-center mx-40">
+        <div className="transform -rotate-90 md:rotate-0 text-2xl font-semibold text-gray-900 m-auto md:m-0 md:mt-8 text-left">
+          <p>에러가 발생했습니다. 나중에 다시 시도해주세요.</p>
+        </div>
+      </div>
+    );
+  } else if (data) {
+    const blogCards = data["posts"].map((blog) => (
+      <BlogCard
+        title={blog["title"]}
+        preface={blog["preface"]}
+        url={blog["url"]}
+        author={blog["author"]}
+      />
+    ));
+
+    mainComponent = (
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 justify-items-center mx-40">
+        {blogCards}
+      </div>
+    );
+  }
+
   return (
     <AppLayout>
       <Intro
         title="AUSG Blog"
         description="AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요"
       />
-      {/* <div className="grid-cols-4 sm:grid-cols-1"> */}
 
-      {/* <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12"> */}
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 justify-items-center mx-40">
-        <BlogCard
-          title="asdjkhaskdjhas"
-          preface="AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 "
-          url={"https://velog.io/"}
-          author="문성혁"
-        />
-        <BlogCard
-          title="asdjkhaskdjhas"
-          url={"https://medium.com/"}
-          author="문성혁"
-        />
-        <BlogCard
-          title="asdjkhaskdjhas"
-          preface="AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 "
-          url={"https://velog.io/"}
-          author="문성혁"
-        />
-        <BlogCard
-          title="asdjkhaskdjhas"
-          preface="AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 "
-          url={"https://velog.io/"}
-          author="문성혁"
-        />
-        <BlogCard
-          title="asdjkhaskdjhas"
-          preface="AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 "
-          url={"https://velog.io/"}
-          author="문성혁"
-        />
-        <BlogCard
-          title="asdjkhaskdjhas"
-          preface="AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 "
-          url={"https://velog.io/"}
-          author="문성혁"
-        />
-        <BlogCard
-          title="asdjkhaskdjhas"
-          preface="AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 "
-          url={"https://asd.io/"}
-          author="문성혁"
-        />
-        <BlogCard
-          title="asdjkhaskdjhas"
-          preface="AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 "
-          url={"https://roeniss.tistory.com/"}
-          author="문성혁"
-        />
-        <BlogCard
-          title="asdjkhaskdjhas"
-          preface="AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 AUSG 팀원들이 작성한 블로그 게시글을 둘러보세요 "
-          url={"https://velog.io/"}
-          author="문성혁"
-        />
-        {/* </div> */}
-      </div>
-      {/* </div> */}
+      {/*  */}
+      {mainComponent}
     </AppLayout>
   );
 };
