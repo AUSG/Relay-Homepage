f4483185f7df152eca1cee1958d04ef365d0861a
suyeon96 <dntndus9611@gmail.com>
2021-05-30 19:06:57 +0900

    update meta tag & tracking GA

--- client/src/components/common/Layout.tsx
+++ client/src/components/common/Layout.tsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import React from "react";
 
-export const siteTitle = "AUSG - AWSkrug University Student Group";
+export const siteTitle = "AUSG";
 export const siteDescription =
-  "안녕하세요. 우리는 AUSG(AWSkrug University Student Group)입니다. 우리는 AWS와 클라우드에 관심이 많은 사람들과 함께 하고 있습니다.";
+  "AUSG(AWSKRUG University Student Group) 공식 홈페이지 입니다.";
+export const GA_TRACKING_ID = "G-G8WPXP0PB2";
 
 interface LayoutProps {
   home?: boolean;
@@ -27,32 +28,50 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
         />
+        <title>{siteTitle}</title>
+
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="robots" content="ALL" />
+        <meta name="robots" content="index, follow"/>
+        <meta name="title" content={siteTitle} />
+        <meta name="keywords" content="AUSG" />
         <meta name="description" content={siteDescription} />
-        {/* <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        /> */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="theme-color" content="#000000" />
-        <meta name="og:title" content={siteTitle} />
+        <meta property="og:title" content={siteTitle} />
         <meta property="og:type" content="website" />
         <meta property="og:url" content={url} />
         <meta property="og:description" content={siteDescription} />
         <meta property="og:site_name" content="AUSG Homepage" />
+        <meta property="og:image" content="https://ausg.me/images/ausg-banner.png" />
+
         {/* Naver에서 제공 하는 검색 어드바이저 https://searchadvisor.naver.com/ */}
-        <meta
-          name="naver-site-verification"
-          content="41c30f24dff3c33c26a870cc7d4d392d3e2cebdb"
-        />
+        <meta name="naver-site-verification" content="e09e694c50c5fad12e5bce73f2468d8c9a2aaebd" />
+        <meta name="naver-site-verification" content="41c30f24dff3c33c26a870cc7d4d392d3e2cebdb" />
         <link href="/images/logo-black.svg" rel="icon" />
-        <title>{siteTitle}</title>
+
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+            `
+          }}
+        />
       </Head>
-      <header></header>
+      <header>
+      </header>
       <main>{children}</main>
     </div>
   );
