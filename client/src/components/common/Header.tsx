e3b6ac68db66288a5ec78a9978d7433c6b8c1090
Ray <gmj1197@gmail.com>
2021-05-31 00:43:17 +0900

    Revert "feat(client): active state in header link button"
    This reverts commit cf4b183352470d7657b770873d58cff1d57795c7.

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -1,12 +1,8 @@
-import { css } from "@emotion/react";
 import styled from "@emotion/styled";
-import { useRouter } from "next/dist/client/router";
 import Link from "next/link";
 import React from "react";
 
 const Header: React.FC = () => {
-  const router = useRouter()
-
   return (
     <div className="header m-header">
       <div className="doc_header flex-none px-16">
@@ -24,13 +20,13 @@ const Header: React.FC = () => {
         <Space />
         <Nav>
           <Link href="/blog" as="/blog">
-            <NavItem active={router.pathname.indexOf('/blog') === 0}>BLOG</NavItem>
+            <NavItem>BLOG</NavItem>
           </Link>
           <Link href="/people" as="/people">
-            <NavItem active={router.pathname.indexOf('/people') === 0}>PEOPLE</NavItem>
+            <NavItem>PEOPLE</NavItem>
           </Link>
           <Link href="/contact" as="/contact">
-            <NavItem active={router.pathname.indexOf('/contact') === 0}>CONTACT</NavItem>
+            <NavItem>CONTACT</NavItem>
           </Link>
         </Nav>
       </div>
@@ -44,20 +40,15 @@ const Space = styled.div`
 
 const Nav = styled.div``;
 
-const NavItem = styled.a<{ active?: boolean }>`
+const NavItem = styled.a`
   font-size: 0.875rem;
-  color: #ced4da;
+  color: #212529;
   margin-left: 1rem;
   cursor: pointer;
-  transition: color 200ms;
 
   &:hover {
     text-decoration: none;
   }
-  ${(props) => props.active && css`
-    color: #212529;
-    font-weight: 500;
-  `}
 `;
 
 export default Header;
