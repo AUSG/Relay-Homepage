9a62189431f0b07bd922ec2d8391abd85f1838ad
yebonkim <v954242@gmail.com>
2021-05-30 23:24:21 +0900

    feat(client): active state in header link button

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -1,8 +1,12 @@
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+import { useRouter } from "next/dist/client/router";
 import Link from "next/link";
 import React from "react";
 
 const Header: React.FC = () => {
+  const router = useRouter()
+
   return (
     <div className="header m-header">
       <div className="doc_header flex-none px-16">
@@ -20,13 +24,13 @@ const Header: React.FC = () => {
         <Space />
         <Nav>
           <Link href="/blog" as="/blog">
-            <NavItem>BLOG</NavItem>
+            <NavItem active={router.pathname.indexOf('/blog') === 0}>BLOG</NavItem>
           </Link>
           <Link href="/people" as="/people">
-            <NavItem>PEOPLE</NavItem>
+            <NavItem active={router.pathname.indexOf('/people') === 0}>PEOPLE</NavItem>
           </Link>
           <Link href="/contact" as="/contact">
-            <NavItem>CONTACT</NavItem>
+            <NavItem active={router.pathname.indexOf('/contact') === 0}>CONTACT</NavItem>
           </Link>
         </Nav>
       </div>
@@ -40,15 +44,20 @@ const Space = styled.div`
 
 const Nav = styled.div``;
 
-const NavItem = styled.a`
+const NavItem = styled.a<{ active?: boolean }>`
   font-size: 0.875rem;
-  color: #212529;
+  color: #ced4da;
   margin-left: 1rem;
   cursor: pointer;
+  transition: color 200ms;
 
   &:hover {
     text-decoration: none;
   }
+  ${(props) => props.active && css`
+    color: #212529;
+    font-weight: 500;
+  `}
 `;
 
 export default Header;
