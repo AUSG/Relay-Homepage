c3404a3688e542d880f44d8a1222f6b42f209aba
suyeon96 <dntndus9611@gmail.com>
2021-05-30 19:30:50 +0900

    update mainpage content

--- client/src/components/common/Content.tsx
+++ client/src/components/common/Content.tsx
@@ -17,27 +17,21 @@ const Content: React.FC<ContentProps> = () => (
     <div className="doc_content">
       <div>
         <div className="title">
-          <strong> AWSkrug University Student Group </strong>
+          <strong> AWSKRUG University Student Group </strong>
         </div>
         <div className="description">
           <ul>
-            <li> AUSG는 AWS와 클라우드, 프로그래밍에 관심을 갖고 </li>
-            <li> 이와 관련 주제들로 이야기를 나누고 </li>
-            <li> 함께 성장하며 나아가는 대학생들의 모임입니다. </li>
+            <li> AUSG는 AWS와 Cloud, 프로그래밍에 열정있는 대학생들이 모여 </li>
+            <li> 이와 관련 주제들로 이야기를 나누고, 함께 성장하며 나아가는</li>
+            <li> AWSKRUG University Student Group 입니다. </li>
+          </ul>
+          <ul>
+            <li> AUSG에서는 대학생부터 현업에서 근무하는 개발자까지 </li>
+            <li> 다양한 사람들을 만나 자유로운 분위기에서 각자의 경험을 나누고 소통할 수 있습니다. </li>
           </ul>
           <ul>
             <li>
-              AUSG에서는 대학교에서 공부하는 친구들부터 현업에서 근무하는
-              개발자도 함께
-            </li>
-            <li> 다양한 사람들을 만나 소통하실 수 있습니다. </li>
-            <li> 함께 성장하며 나아가는 대학생들의 모임입니다. </li>
-          </ul>
-
-          <ul>
-            <li>
-              AWS 한국사용자모임(https://awskrug.github.io/)과 함께하고 있어요!
-              😎
+              AWS 한국사용자모임(https://awskrug.github.io/)과 함께하고 있어요! 😎
             </li>
           </ul>
         </div>
@@ -49,11 +43,9 @@ const Content: React.FC<ContentProps> = () => (
             <strong> Our Vision </strong>
           </div>
           <ul className="square_list_type">
-            <li> 우리는 각자의 경험을 나누고 함께 발전하는것을 좋아합니다. </li>
+            <li> 우리는 각자의 경험을 나누고 함께 성장하는 것을 추구합니다. </li>
             <li> 막히는 것이 있다면 언제든 편하게 같이 이야기합니다. </li>
-            <li>
-              각자의 자리에서 가진 고민들을 함께 나누며 성장하고 있습니다.
-            </li>
+            <li> 각자의 자리에서 가진 고민들을 함께 나누며 발전하고 있습니다. </li>
             <li> 앗, 물론 개발만이 아니라 함께 놀기도 합니다!! </li>
           </ul>
         </div>
@@ -84,19 +76,11 @@ const Content: React.FC<ContentProps> = () => (
         </div>
         <div className="description">
           <ul>
-            <li>
-              우리는 AWS에 대한 각자의 경험을 나누고, 그 속에서 매 순간 성장하고
-              있습니다.
-            </li>
-            <li>아래는 저희가 경험했던 다양한 행사들입니다.</li>
-            <li>
-              이 외에도 아키텍처, 자격증, 기술 서적 등을 기반으로 스터디를
-              진행하며 성장을 추구하고 있으며,
-            </li>
-            <li>
-              숙명여대 해커톤에 클라우드 어시스턴스로 참여, AWSKRUG와 함께 AWS
-              re:Invent 참관 등 많은 활동을 해오고 있습니다.
-            </li>
+            <li> 우리는 AWS에 대한 각자의 경험을 나누고, </li>
+            <li> 스터디 · 프로젝트 · 세미나 · 외부동아리와의 연계 등 다양한 활동을 진행합니다. </li>
+            <li> 아래는 저희가 경험했던 다양한 행사들입니다. </li>
+            <li> 이 외에도 아키텍처, 자격증, 기술 서적 등을 기반으로 상시 스터디를 진행하며 성장을 추구하고 있으며, </li>
+            <li> 숙명여대 해커톤 클라우드 어시스턴스 참여, AWS re:Invent 참관 등 많은 활동을 해오고 있습니다.</li>
           </ul>
         </div>
       </div>
@@ -110,24 +94,11 @@ const Content: React.FC<ContentProps> = () => (
         </div>
         <div className="description">
           <ul>
-            <li>
-              천천히. 꾸준하게. 자기{" "}
-              <span style={{ fontSize: "24px", fontWeight: "bold" }}>
-                자신의 성장
-              </span>
-              나아가는
-              <span style={{ fontSize: "24px", fontWeight: "bold" }}>열정</span>
-            </li>
-            <li>우리는 꾸준히 스스로에 발전을 지향합니다.</li>
-            <li>
-              각자의 관심사에 맞는 스터디를 합니다. 이를 기반으로 프로젝트를
-              진행합니다.
-            </li>
-            <li>
-              외부 해커톤 및 세미나 지원 예정이며, 이번 시즌 저희는
-              <span style={{ fontSize: "16px", fontWeight: "bold" }}>SOPT</span>
-              와 함께합니다.
-            </li>
+            <li> 지식의 나눔. 경험의 공유. 천천히 하지만 꾸준한 성장. 앞으로 나아가는 열정. </li>
+            <li> . </li>
+            <li> 각자 관심있는 주제가 생기면 스터디를 선행하고, 이를 기반으로 프로젝트를 진행합니다. </li>
+            <li> 외부 해커톤 및 각종 세미나에 스피커 또는 서포터로 함께 할 예정이며, </li>
+            <li> 이번 시즌 저희는 <span style={{ fontSize: "16px", fontWeight: "bold" }}>SOPT</span> 와 함께합니다. </li>
           </ul>
         </div>
       </div>
@@ -143,12 +114,15 @@ const Content: React.FC<ContentProps> = () => (
                 이런 분들과 함께 하고자 함께 하고 싶습니다!
               </span>
             </li>
-            <li>
-              AWS 또는 클라우드에 관심있는 대한민국 소재 대학 재학생 및
-              휴학생이라면 누구나 지원 하실 수 있습니다.
-            </li>
-            <li>함께 개발자들과 즐거운 커뮤니케이션을 즐기는 사람!</li>
-            <li>꾸준히 스스로 발전을 추구하는 사람!</li>
+            <li> AWS를 비롯한 클라우드에 관심이 많은 대한민국 소재 대학 재학생 및 휴학생! </li>
+            <li> 지식의 나눔을 추구하며, 적극적으로 활동이 가능한 사람! </li>
+            <li> 다양한 개발자들과의 커뮤니케이션을 즐기는 사람! </li>
+            <li> 꾸준히 스스로의 발전을 추구하는 사람! </li>
+            <li> . </li>
+            <li> . </li>
+            <li> <span style={{ fontSize: "20px", fontWeight: "bold" }}>지원서 제출</span> </li>
+            <li> 2021.06.20(일) 23:59 까지 </li>
+            <li> <a href="http://bit.ly/ausg-5th"> ▶ 5th AUSG 지원하기 ◀ </a> </li>
           </ul>
         </div>
       </div>
