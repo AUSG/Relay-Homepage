37c78c5bbc43eb1c51af52e928d4c29ac4d295bd
yebonkim <v954242@gmail.com>
2021-05-30 23:32:39 +0900

    fix(client): remove unused variable

--- client/src/pages/blog.tsx
+++ client/src/pages/blog.tsx
@@ -7,7 +7,7 @@ import useAxios from "axios-hooks";
 import ReactLoading from "react-loading";
 
 const BlogPage: NextPage = () => {
-  const [{ data, loading, error }, _refetch] = useAxios(
+  const [{ data, loading, error }] = useAxios(
     "https://raw.githubusercontent.com/AUSG/Relay-Homepage/feature/blogPage/lib/blogSpider/newestPosts.json",
     { ssr: false },
   );
