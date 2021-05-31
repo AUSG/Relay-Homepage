2d4c212801e2b085ffb99e72d7657fec12b90c86
Roeniss Moon <roeniss2@gmail.com>
2021-05-31 21:17:53 +0900

    feat: mobile compatibility on Contact page

--- client/src/pages/contact.tsx
+++ client/src/pages/contact.tsx
@@ -4,47 +4,49 @@ import { NextPage } from "next";
 const ContactPage: NextPage = () => {
   return (
     <AppLayout>
-      <div className="contact">
-        <div className="py-20 px-3 text-black">
-          <h1 className="mb-8 text-4xl font-bold">Contact Us</h1>
+      <div className="contact mx-auto">
+        <div className="max-w-4xl mx-4">
+          <div className="py-20 px-3 text-black">
+            <h1 className="mb-8 text-4xl font-bold">Contact Us</h1>
 
-          <p className="text-base">AUSG와 함께 더 나은 세상을 만들어요</p>
-        </div>
-        <form
-          action="https://formspree.io/f/xgeprzaq"
-          noValidate={true}
-          method="post"
-        >
-          <div className="half">
-            <input
-              className="boxes"
-              type="email"
-              name="_replyto"
-              placeholder="이메일 ( Email )"
-              aria-required={true}
-              data-validation-required-message="Please enter your email address."
-            />
-            <input
-              className="boxes"
-              type="text"
-              name="name"
-              placeholder="이름 ( Name )"
-              aria-required={true}
-              data-validation-required-message="Please enter your name."
-            />
-            <textarea
-              className="boxes"
-              id="message"
-              name="message"
-              placeholder="내용은 이메일로 답변 드립니다. ( Message )"
-              aria-required={true}
-              data-validation-required-message="Please enter a message."
-            ></textarea>
+            <p className="text-base">AUSG와 함께 더 나은 세상을 만들어요</p>
           </div>
-          <button className="sendbutton" type="submit" value="Send">
-            <i className="fa fa-paper-plane"> SEND</i>
-          </button>
-        </form>
+          <form
+            action="https://formspree.io/f/xgeprzaq"
+            noValidate={true}
+            method="post"
+          >
+            <div className="half">
+              <input
+                className="boxes"
+                type="email"
+                name="_replyto"
+                placeholder="연락 받으실 이메일"
+                aria-required={true}
+                data-validation-required-message="Please enter your email address."
+              />
+              <input
+                className="boxes"
+                type="text"
+                name="name"
+                placeholder="보내시는 분 성함"
+                aria-required={true}
+                data-validation-required-message="Please enter your name."
+              />
+              <textarea
+                className="boxes"
+                id="message"
+                name="message"
+                placeholder="무엇이든 편하게 말씀주세요"
+                aria-required={true}
+                data-validation-required-message="Please enter a message."
+              ></textarea>
+            </div>
+            <button className="sendbutton" type="submit" value="Send">
+              <i className="fa fa-paper-plane"> SEND &nbsp;</i>
+            </button>
+          </form>
+        </div>
       </div>
     </AppLayout>
   );
