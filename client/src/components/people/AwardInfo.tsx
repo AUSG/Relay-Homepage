de77033f59ca95e856471747fa364e5d4e5c683e
roeniss.milk <roeniss.milk@kakaocorp.com>
2021-09-19 14:24:42 +0900

    어워드 파일 추가:

---
+++ client/src/components/people/AwardInfo.tsx
@@ -0,0 +1,24 @@
+const awardCatalog = {
+  "ex)_간단한_타이틀과_날짜(mmdd)_또는_기간(mmdd_mmdd)": {
+    iconFile: "fileName.svg",
+    description: "이 날엔 이러이러한 활동을 했어요",
+  },
+  baekak_mountain_0911: {
+    iconFile: "/images/icons/mountain.svg",
+    description: "백악산 정도는 산책이지 (9/11)",
+  },
+  tetris_gold_0922: {
+    iconFile: "/images/icons/tetris_gold.svg",
+    description: "한가위 테트리스 올림픽 금메달 (9/22)",
+  },
+  tetris_silver_0922: {
+    iconFile: "/images/icons/tetris_silver.svg",
+    description: "한가위 테트리스 올림픽 은메달 (9/22)",
+  },
+  tetris_bronze_0922: {
+    iconFile: "/images/icons/tetris_bronze.svg",
+    description: "한가위 테트리스 올림픽 동메달 (9/22)",
+  },
+};
+
+export default awardCatalog;
