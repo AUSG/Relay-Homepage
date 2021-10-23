ec1f7367454e2fd67df980923099e13190b5330e
Roeniss Moon <roeniss2@gmail.com>
2021-10-24 01:10:29 +0900

    연도를 description에서 제거 (중복이 너무 심함)

--- client/src/components/people/AwardInfo.tsx
+++ client/src/components/people/AwardInfo.tsx
@@ -5,23 +5,23 @@ const awardCatalog = {
   },
   "2021_baekak_mountain": {
     iconFile: "/images/icons/mountain.svg",
-    description: "2021 백악산 정도는 산책이지",
+    description: "백악산 정도는 산책이지",
   },
   "2021_chuseok_tetris_gold": {
     iconFile: "/images/icons/tetris_gold.svg",
-    description: "2021 한가위 테트리스 올림픽 금메달",
+    description: "한가위 테트리스 올림픽 금메달",
   },
   "2021_chuseok_tetris_silver": {
     iconFile: "/images/icons/tetris_silver.svg",
-    description: "2021 한가위 테트리스 올림픽 은메달",
+    description: "한가위 테트리스 올림픽 은메달",
   },
   "2021_chuseok_tetris_bronze": {
     iconFile: "/images/icons/tetris_bronze.svg",
-    description: "2021 한가위 테트리스 올림픽 동메달",
+    description: "한가위 테트리스 올림픽 동메달",
   },
   "2021_chuseok_tetris_oops": {
     iconFile: "/images/icons/tetris_oops.svg",
-    description: "2021 한가위 테트리스 올림픽 아차상",
+    description: "한가위 테트리스 올림픽 아차상",
   },
   "2021_book_reading": {
     iconFile: "/images/icons/book.png",
