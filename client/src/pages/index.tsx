c770edf10a58fac7c7f11fb758498706858b9f10
naru200 <jinsu2504@gmail.com>
2022-03-07 04:59:03 +0900

    Apply google analytics (by 배진수)

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,6 +2,7 @@ import Banner from "@ausg/components/common/Banner";
 import Content from "@ausg/components/common/Content";
 import AppLayout from "@ausg/components/templates/AppLayout";
 import { consoleEasterEgg } from "@ausg/easterEggs/consoleEasterEgg";
+import Head from "next/head";
 import React from "react";
 
 const Home: React.FC = () => {
@@ -11,6 +12,19 @@ const Home: React.FC = () => {
 
   return (
     <AppLayout>
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-LV39T61R82"
+        />
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-LV39T61R82');`}
+        </script>
+      </Head>
       <Banner />
       <Content />
     </AppLayout>
